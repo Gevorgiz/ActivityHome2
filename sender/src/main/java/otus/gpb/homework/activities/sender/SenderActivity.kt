8492605f520fd834de9a12e8faa2577e5b260551
Gevorgiz <114309665+Gevorgiz@users.noreply.github.com>
2024-01-15 22:13:30 +0300

    Activity_home2

--- sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
+++ sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
@@ -1,9 +1,11 @@
 package otus.gpb.homework.activities.sender
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import java.io.Serializable
 
 
 
@@ -25,10 +27,30 @@ class SenderActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
+        buttonGoogleMaps.setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_VIEW, Uri.parse(
+                    ("geo:0,0?q=restaurants")
+
+                )
+            )
+            startActivity(intent)
+        }
+
+        buttonSendMail.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.data = Uri.parse("mailto:android@otus.ru")
+            intent.type = "text/plan"
+
+            startActivity(intent)
+        }
+
+
         buttonOpenReceiver.setOnClickListener {
 
 
-            val intent = Intent(this, ReceiverActivity::class.java).apply {
+            val intent = Intent(this, SenderActivity::class.java).apply {
 
                 putExtra("title", "string")
                 putExtra("year", "string")
@@ -38,7 +60,6 @@ class SenderActivity : AppCompatActivity() {
             startActivity(intent)
 
         }
-
 
     }
 }
