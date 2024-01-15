9848709309ad1a7b3f368047d2a461d3487933db
Gevorgiz <114309665+Gevorgiz@users.noreply.github.com>
2024-01-15 12:16:24 +0300

    Crate activity

--- receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
+++ receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
@@ -1,12 +1,18 @@
 package otus.gpb.homework.activities.receiver
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+
+private const val TAG = "ReceiverActivity"
 
 class ReceiverActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receiver)
+
+        val key = intent.extras?.getString("KEY")
+        Log.d(TAG, "extra key:$key")
     }
 }
